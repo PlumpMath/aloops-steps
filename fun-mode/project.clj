df391bdf8627055f37ce84b01ebc6f4f76a6f103
Esperanza <hola@mi-mina.com>
2016-07-26 17:40:31 +0200

    add fun-mode

---
+++ fun-mode/project.clj
@@ -0,0 +1,10 @@
+(defproject fun-mode "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [de.sojamo/oscp5 "0.9.8"]
+                 [quil "2.4.0-osc"]]
+  :main fun-mode.core)
+
